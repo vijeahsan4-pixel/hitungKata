fa1445eb8aaa83e4979cd08181b7d0b8114119c1
vijeahsan4-pixel <vijeahsan4@gmail.com>
2026-02-12 10:58:15 +0700

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        TextBox2 = New TextBox()
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(26, 22)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(213, 50)
+        Button1.TabIndex = 0
+        Button1.Text = "Pilih File dan Analisis"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(26, 92)
+        TextBox1.Name = "TextBox1"
+        TextBox1.ReadOnly = True
+        TextBox1.Size = New Size(213, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(26, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(76, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "Hasil Analisis"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(26, 40)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.ScrollBars = ScrollBars.Vertical
+        TextBox2.Size = New Size(213, 36)
+        TextBox2.TabIndex = 4
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Location = New Point(18, 31)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(532, 138)
+        GroupBox1.TabIndex = 5
+        GroupBox1.TabStop = False
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Label1)
+        GroupBox2.Controls.Add(TextBox2)
+        GroupBox2.Location = New Point(18, 175)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(532, 138)
+        GroupBox2.TabIndex = 6
+        GroupBox2.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Aplikasi Hitung Kata"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ' Procedure (Sub) untuk menghitung jumlah kata dalam sebuah string
+    Private Sub HitungKata(ByVal teks As String, ByRef totalKata As Integer, ByRef kataPanjang As Integer)
+        ' Split teks menjadi array kata berdasarkan spasi
+        Dim kataArray() As String = teks.Split({" "c}, StringSplitOptions.RemoveEmptyEntries)
+
+        ' Perulangan: Loop melalui setiap kata
+        For Each kata As String In kataArray
+            totalKata += 1  ' Hitung total kata
+
+            ' Percabangan: Jika panjang kata > 5, hitung sebagai kata panjang
+            If kata.Length > 5 Then
+                kataPanjang += 1
+            End If
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Membuat OpenFileDialog untuk memilih file
+        Dim openFileDialog As New OpenFileDialog
+        openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+        openFileDialog.Title = "Pilih File Teks"
+
+        If openFileDialog.ShowDialog = DialogResult.OK Then
+            ' Tampilkan path file di TextBox1
+            TextBox1.Text = openFileDialog.FileName
+
+            ' Membaca file (menggunakan StreamReader)
+            Dim totalKata = 0
+            Dim kataPanjang = 0
+
+            Try
+                Using reader As New StreamReader(openFileDialog.FileName)
+                    Dim line As String
+                    ' Perulangan: Baca baris per baris
+                    Do While reader.Peek >= 0
+                        line = reader.ReadLine
+                        ' Panggil procedure untuk menghitung kata di setiap baris
+                        HitungKata(line, totalKata, kataPanjang)
+                    Loop
+                End Using
+
+                ' Tampilkan hasil di TextBox2
+                TextBox2.Text = $"Total Kata: {totalKata}" & vbCrLf &
+                                $"Kata dengan Panjang > 5 Huruf: {kataPanjang}"
+            Catch ex As Exception
+                MessageBox.Show("Error membaca file: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
